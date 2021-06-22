a86eab85fd000a441116fe7ffd3c6dc62c6f1da9
RodrigoJesusSantistebanPachari <58257135+RodrigoJesusSantistebanPachari@users.noreply.github.com>
2021-06-22 00:49:03 -0500

    Update extra.h

--- extra.h
+++ extra.h
@@ -1,11 +1,41 @@
-
-bool contains(Token t, std::vector<Token> container)
+template <class Tipo>
+bool contains(Tipo t, std::vector<Tipo> container)
 {
     for (int k = 0; k < container.size(); k++)
     {
-        auto v = container[k];
         if (container[k] == t)
             return true;
     }
     return false;
+}
+
+template <class Tipo>
+void print(std::vector<Tipo> container)
+{
+    for (int k = 0; k < container.size(); k++)
+    {
+        std::cout << container[k].toString() << "\n";
+    }
+    std::cout << "\n";
+}
+
+State dummyStartState(std::string S)
+{
+    Nodo leftSide{"TOP", NonTerminal};
+    std::vector<Nodo> rightSide;
+    Nodo n{S, NonTerminal};
+    rightSide.push_back(n);
+
+    State dummyStartState{leftSide, rightSide, 0};
+    dummyStartState.setIdx1(0);
+    dummyStartState.setIdx2(0);
+    dummyStartState.setPointIdx(0);
+
+    return dummyStartState;
+}
+
+
+//To be implemented
+void setProbability(){
+	
 }
