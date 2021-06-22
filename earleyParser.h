175e570790f9c279b4f653b63f9bc1757cee122c
gabbichipana <42560116+gabbichipana@users.noreply.github.com>
2021-06-21 21:50:44 -0500

    2 commit
    Completer

--- earleyParser.h
+++ earleyParser.h
@@ -3,20 +3,21 @@ class EarleyParser
 private:
     Grammar grammar;
     std::string text;
-    std::vector<std::string> words;
+    std::vector<std::string> words; 
     Chart chart;
 public:
     EarleyParser() = default;
     EarleyParser(Grammar g, std::string text);
     void PREDICTOR(State s);
     void SCANNER(State s);
-    void COMPLETER(State s, int idx); // Aun en elaboracion
+    void COMPLETER(State s);
     void setGrammar(std::string fileName);
     void setWords();
     void setChart(int n);
     void setText();
     void printChart();
     void printGrammar();
+    bool process();
     ~EarleyParser();
 };
 
@@ -24,39 +25,104 @@ EarleyParser::EarleyParser(Grammar g, std::string text)
 {
     this->grammar = g;
     this->text = text;
-    //dummy start state
 }
+
+/*
+procedure PREDICTOR((A â†’ â€¦ @ B â€¦, [i, j]))
+    for each (B â†’ RHS) in grammar do
+    chart[j].pushback(B â†’ @ RHS, [j, j]) 
+*/
 
 void EarleyParser::PREDICTOR(State s)
 {
     int j = s.getIdx2();
-    Token t = s.nextElement();
+    Nodo t = s.nextElement();
     std::vector<Production> prods = grammar.getProductions();
     State temp;
     for (int k = 0; k < prods.size(); k++)
     {
-        if ((prods[k].getLeftSide().getValue() == t.getValue()) && (prods[k].getLeftSide().getType() == t.getType()))
+        if ((prods[k].getLeftSide() == t))
         {
             temp = prods[k].convertToState(0);
             temp.setIdx1(j);
             temp.setIdx2(j);
-            chart.getContent()[j].push_back(temp);
+            
+            if(!contains<State>(temp, chart.content[j]))
+                chart.content[j].push_back(temp);
         }
     }
 }
 
-void EarleyParser::SCANNER(State s)
+
+/* 
+Espacio para el scanner 
+*/ 
+
+
+
+
+
+/*
+procedure SCANNER((A â†’ â€¦ @ L â€¦, [i, j]))
+    if (L â†’ word[j]) is_in grammar
+    chart[j + 1].pushback(L â†’ ...word[j] @ ..., [j, j + 1])
+*/
+
+
+void EarleyParser::SCANNER(State s) // indices [i, j]
 {
     State temp = s;
     int j = temp.getIdx2();
-    Token t = temp.nextElement();
-    std::vector<Production> prods = grammar.getProductions();
-    if (t.getType() ==  Terminal)
+    Nodo t = temp.nextElement();
+
+    Production prod;
+    prod.setLeftSide(t);
+    Nodo tempNodo {words[j], Terminal};
+    std::vector<Nodo> tempVector;
+    tempVector.push_back(tempNodo);
+    prod.setRightSide(tempVector);
+
+    if ( ( contains<Production>(prod, this->grammar.getProductions()) ) ||
+         (t.getType() ==  Terminal && t.getValue() == words[j] ) )
     {
         temp.move();
         temp.setIdx1(j);
         temp.setIdx2(j+1);
-        chart.getContent()[j+1].push_back(temp);
+        chart.content[j+1].push_back(temp);
+    }
+}
+
+
+
+/*
+procedure COMPLETER((B â†’ â€¦ @, [j, k]))
+    foreach state in chart[j] :
+        if state LIKE (A â†’ â€¦ @ B â€¦, [i, j])
+            chart[k].pushback(A â†’ â€¦ B @ â€¦, [i, k])
+end
+*/
+
+void EarleyParser::COMPLETER(State s) // indices [j, k]
+{
+    State eachState;
+    int index;
+    std::vector<State> temp = chart.content[s.getIdx1()];  //  chart[j]
+    std::string leftSide = s.getLeftSide().getValue();          // "B"
+    
+    for (int k = 0; k < temp.size(); k++ )
+    {
+        eachState = temp[k];
+        index = eachState.getPointIdx()+1;
+        if (eachState.getRightSide().size() > index)
+        {
+            if( eachState.getRightSide()[index].getValue() == leftSide )
+            {
+                eachState.move();
+                eachState.setIdx2(s.getIdx2());
+                if ( !contains<State> (eachState, chart.content[s.getIdx2()]) )
+                    chart.content[s.getIdx2()].push_back(eachState);
+            }
+        }
     }
 }
 
@@ -91,10 +157,10 @@ void EarleyParser::setWords()
 void EarleyParser::setChart(int n)
 {
     chart.setUpChart(n);
-    chart.setContent(this->words);
+    //chart.setContent(this->words);
 }
 
-void EarleyParser::setText()
+void EarleyParser::setText() 
 {
     std::cout<< "\nIngrese una cadena para analizarla:\t";
     std::getline(std::cin, this->text);
@@ -112,6 +178,43 @@ void EarleyParser::printChart()
 {
     std::cout<< chart.toText();
 }
+
+
+// Main Function
+bool EarleyParser::process()
+{
+    //add dummy start state
+    State DSS = dummyStartState(this->grammar.getInitial());
+    chart.content[0].push_back(DSS);
+
+    for (int k = 0; k < words.size()+1; k++) // por cada chart[i]
+    {
+        for (int p = 0; p < chart.content[k].size(); p++)
+        {
+            if (chart.content[k][p].isIncomplete())
+            {
+                 if (chart.content[k][p].nextElement().getType() == NonTerminal)
+                    PREDICTOR(chart.content[k][p]);
+                else if (chart.content[k][p].nextElement().getType() == Terminal)
+                    SCANNER(chart.content[k][p]);
+            }
+            else
+                COMPLETER(chart.content[k][p]);
+         }
+    }
+    
+    std::vector<Nodo> nodos;
+    nodos.push_back(Nodo{grammar.getInitial(), NonTerminal});
+    nodos.push_back(Nodo{".", Point});
+    State Top {Nodo{"TOP", NonTerminal}, nodos};
+
+    if(contains<State>(Top, chart.content[chart.content.size()-1]))
+        return true;
+    else
+        return false;
+}
+
+
 
 EarleyParser::~EarleyParser()
 {
