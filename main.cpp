c003127450ee3ad637a6756d94aaf376a5f4b9d9
gabbichipana <42560116+gabbichipana@users.noreply.github.com>
2021-07-13 10:57:20 -0500

    commit3
    probabilities and index management

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
-#include <string.h>
+#include <cstring>
 #include "nodo.h"
 #include "state.h"
 #include "extra.h"
@@ -10,22 +10,14 @@
 #include "chart.h"
 #include "earleyParser.h"
 
+
 int main()
 {
     EarleyParser EP;
     EP.setGrammar("grammar.txt");
     EP.printGrammar();
-
-    //Se setean de forma automática las probabilidades de acuerdo a cada producción de la gramática
-    EP.setProbabilities();
-
-    //Saltamos una línea para leer el texto
-    std::cin.ignore();
-
-    //Se lee la cadena a ser reconocida
+    EP.convertToProbabilisiticParser();
     EP.setText(); 
-
-    //Se procesa
     bool ACCEPTED = EP.process();
     EP.printChart();
 
@@ -33,9 +25,6 @@ int main()
         std::cout<< "Cadena aprobada\n\n";
     else
         std::cout<< "Cadena NO aprobada\n\n";
-    
-    
+
     return 0;
 }
-
-
